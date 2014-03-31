619f394ea56d3fdf7d9218020dd81d45bc3c593b
hexuotzo <hexuotzo@gmail.com>
2014-03-31 12:23:28 +0800

    修改签到url...

--- xiami/xiami.py
+++ xiami/xiami.py
@@ -1,17 +1,23 @@
 # -*- coding:utf-8 -*-
 import mechanize
 LOGIN_URL = "http://www.xiami.com/web/login"
+CHECKIN_URL = "http://www.xiami.com/web"
 
-def main():    
+def login():    
     br = mechanize.Browser()
     br.set_handle_robots(False)
     br.addheaders = [('User-agent', 'Firefox')]
+    
+    #login
     br.open(LOGIN_URL)
     br.select_form(nr=0)
     br['email'] = ''
     br['password'] = ''
     br.submit()
-    check_in = br.follow_link(text_regex = '签到')
+    
+    #checkin
+    br.open(CHECKIN_URL)
+    check_in = br.follow_link(text_regex='每日签到')
     print check_in.read()
 
 if __name__ == '__main__':
