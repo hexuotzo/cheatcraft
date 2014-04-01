ab2de9fda86861e2d897cd0c56e4d7dcc80f0e62
hexuotzo <hexuotzo@gmail.com>
2014-04-01 09:48:21 +0800

    fix typo

--- xiami/xiami.py
+++ xiami/xiami.py
@@ -3,18 +3,18 @@ import mechanize
 LOGIN_URL = "http://www.xiami.com/web/login"
 CHECKIN_URL = "http://www.xiami.com/web"
 
-def login():    
+def main():
     br = mechanize.Browser()
     br.set_handle_robots(False)
     br.addheaders = [('User-agent', 'Firefox')]
-    
+
     #login
     br.open(LOGIN_URL)
     br.select_form(nr=0)
     br['email'] = ''
     br['password'] = ''
     br.submit()
-    
+
     #checkin
     br.open(CHECKIN_URL)
     check_in = br.follow_link(text_regex='每日签到')
